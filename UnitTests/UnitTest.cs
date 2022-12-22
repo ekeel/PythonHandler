09d353f9c12c04eb37227141d4a1813d8bff41a2
Eric.Keeling@coxautoinc.com <Eric.Keeling@coxautoinc.com>
2022-12-22 11:58:16 -0700

    Updated testing

--- UnitTests/UnitTest.cs
+++ UnitTests/UnitTest.cs
@@ -10,7 +10,7 @@ namespace UnitTests
 		}
 
 		[Test]
-		public void BasicTestWithoutReturn()
+		public void RunFromStringWithoutReturn()
 		{
 			Handler pythonHandler = new Handler();
 			pythonHandler.RunFromString("import sys; sysversion = sys.version");
@@ -19,7 +19,7 @@ namespace UnitTests
 		}
 
 		[Test]
-		public void BasicTestWithReturn()
+		public void RunFromStringWithReturn()
 		{
 			Handler pythonHandler = new Handler();
 			var result = pythonHandler.RunFromString<string>("import sys; sysversion = sys.version", "sysversion");
@@ -28,13 +28,45 @@ namespace UnitTests
 		}
 
 		[Test]
-		public void AdditionalVariablesTest()
+		public void RunFromStringWithAdditionalVariablesTest()
 		{
 			var tdict = new Dictionary<string, object>();
 			tdict.Add("testvar", "1ece43cc-b45a-4f97-ba69-5106f23e3932");
 
 			Handler pythonHandler = new Handler();
 			var result = pythonHandler.RunFromString<string>("import sys", "testvar", tdict);
+
+			Assert.IsNotEmpty(result);
+			Assert.That(result, Is.EqualTo("1ece43cc-b45a-4f97-ba69-5106f23e3932"));
+		}
+
+
+		[Test]
+		public void RunFromFileWithoutReturn()
+		{
+			Handler pythonHandler = new Handler();
+			pythonHandler.RunFromFile(@".\resources\test_file.py");
+
+			Assert.Pass();
+		}
+
+		[Test]
+		public void RunFromFileWithReturn()
+		{
+			Handler pythonHandler = new Handler();
+			var result = pythonHandler.RunFromFile<string>(@".\resources\test_file.py", "sysversion");
+
+			Assert.IsNotEmpty(result);
+		}
+
+		[Test]
+		public void RunFromFileWithAdditionalVariablesTest()
+		{
+			var tdict = new Dictionary<string, object>();
+			tdict.Add("testvar", "1ece43cc-b45a-4f97-ba69-5106f23e3932");
+
+			Handler pythonHandler = new Handler();
+			var result = pythonHandler.RunFromFile<string>(@".\resources\test_file.py", "testvar", tdict);
 
 			Assert.IsNotEmpty(result);
 			Assert.That(result, Is.EqualTo("1ece43cc-b45a-4f97-ba69-5106f23e3932"));
